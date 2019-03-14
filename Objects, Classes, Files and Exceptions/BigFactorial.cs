2a17b2855acde1fdf3ac5d9cef68219bf7606591
Kristiyan Dimov <29040168+KristiyanVDimov@users.noreply.github.com>
2019-03-14 11:34:17 +0200

    Create BigFactorial.cs

---
+++ Objects, Classes, Files and Exceptions/BigFactorial.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Big_Factorial
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			BigInteger n = BigInteger.Parse(Console.ReadLine());
+			BigInteger factorial = 1;
+
+			for(int i = 1; i <= n; i++)
+			{
+				factorial *= i;
+			}
+
+			Console.WriteLine(factorial);
+		}
+	}
+}
