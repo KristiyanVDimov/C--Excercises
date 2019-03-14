c6665970c9b216259f123bbcebeb0b06a1cb249c
Kristiyan Dimov <29040168+KristiyanVDimov@users.noreply.github.com>
2019-03-14 11:37:40 +0200

    Create PracticeCharsAndStrings.cs

---
+++ Data Types and Variables/PracticeCharsAndStrings.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Characters_and_Strings
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string str1 = "Software University";
+			char chr1 = 'B';
+			char chr2 = 'y';
+			char chr3 = 'e';
+			string str2 = "I love programming";
+
+			Console.WriteLine($"{str1}\r\n{chr1}\r\n{chr2}\r\n{chr3}\r\n{str2}");
+		}
+	}
+}
