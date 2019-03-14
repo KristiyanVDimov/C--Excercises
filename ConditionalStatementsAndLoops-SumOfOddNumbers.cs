4f6551777cf051b930cd5332421c21704663bf89
Kristiyan Dimov <29040168+KristiyanVDimov@users.noreply.github.com>
2019-03-14 10:30:49 +0200

    Create ConditionalStatementsAndLoops-SumOfOddNumbers.cs

---
+++ ConditionalStatementsAndLoops-SumOfOddNumbers.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Odd_Numbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var n = int.Parse(Console.ReadLine());
+			var current = 1;
+			var sum = 0;
+
+			for(int i = 0; i < n; i++)
+			{
+				Console.WriteLine(current);
+				sum += current;
+				current += 2;
+			}
+			Console.WriteLine("Sum: " + sum);
+		}
+	}
+}
