66ced56121308310cfe8070d9bc1c50f9be4daf8
Kristiyan Dimov <29040168+KristiyanVDimov@users.noreply.github.com>
2019-03-14 11:27:51 +0200

    Create Numbers 1...N with Step 3.java

---
+++ Advanced Loops (Java)/Numbers 1...N with Step 3.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softuni;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Kristiyan Dimov
+ */
+public class Number1toNWithStep3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = Integer.parseInt(s.nextLine());
+        for(int i = 1; i <= n; i += 3){
+            System.out.println(i);
+        }
+    }
+    
+}
