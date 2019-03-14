47bf9522aaddfff4c2dc002d58d5c592d5a66a3c
Kristiyan Dimov <29040168+KristiyanVDimov@users.noreply.github.com>
2019-03-14 11:28:06 +0200

    Create NumbersNto1.java

---
+++ Advanced Loops (Java)/NumbersNto1.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package softuni;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Kristiyan Dimov
+ */
+public class NumbersNto1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = Integer.parseInt(s.nextLine());
+        
+        for(int i = 1; i <= n; n -= i){
+            System.out.println(n);
+        }
+    }
+    
+}
