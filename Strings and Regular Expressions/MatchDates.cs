a67799249248a2b129ea6d70c5f64df6e7ad6e2e
Kristiyan Dimov <29040168+KristiyanVDimov@users.noreply.github.com>
2019-03-14 11:14:20 +0200

    Create MatchDates.cs

---
+++ Strings and Regular Expressions/MatchDates.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"\b(?<day>\d{2})([-.\/])(?<month>[A-Z][a-z]{2})\1(?<year>\d{4})\b";
+
+            var datesStr = Console.ReadLine();
+
+            var dates = Regex.Matches(datesStr, pattern);
+
+            foreach (Match date in dates)
+            {
+                var day = date.Groups["day"].Value;
+                var month = date.Groups["month"].Value;
+                var year = date.Groups["year"].Value;
+                
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
