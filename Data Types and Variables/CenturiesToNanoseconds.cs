17cb7c1e568084811d66889798e10c9cc01151a2
Kristiyan Dimov <29040168+KristiyanVDimov@users.noreply.github.com>
2019-03-14 11:39:13 +0200

    Create CenturiesToNanoseconds.cs

---
+++ Data Types and Variables/CenturiesToNanoseconds.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Centuries_to_Nanoseconds
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int centuries = int.Parse(Console.ReadLine());
+			int years = centuries * 100;
+			int days = (int)(years * 365.2422);
+			int hours = 24 * days;
+			long minutes = (long)hours * 60;
+			ulong seconds = (ulong)minutes * 60;
+			ulong milliseconds = (ulong)seconds * 1000;
+			decimal microseconds = milliseconds * 1000;
+			decimal nanoseconds = microseconds * 1000;
+			Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = {seconds}" +
+				$" seconds = {milliseconds} milliseconds = {microseconds} microseconds = {nanoseconds} nanoseconds");
+		}
+	}
+}
